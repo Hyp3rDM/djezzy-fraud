131cf09f8756c94a0a2790df91a5e1caffc02945
arhvnn <a.arhun@proton.me>
2025-01-28 17:44:48 +0100

    added data cleaning

--- src/DataCleaning.py
+++ src/DataCleaning.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 fraud = pd.read_csv("../data/fraud.csv", delimiter=";")
 non_fraud = pd.read_csv("../data/non-fraud.csv", delimiter=";")
 
-# Enforcing Data Types
 fraud.replace("?", np.nan, inplace=True)
 non_fraud.replace("?", np.nan, inplace=True)
 
@@ -25,3 +22,15 @@ non_fraud.DESTINATION_CAT = non_fraud.DESTINATION_CAT.astype('category')
 non_fraud.MU_HANDSET_DUAL_SIM = non_fraud.MU_HANDSET_DUAL_SIM.astype('category')
 non_fraud.MU_Device_type_Segment = non_fraud.MU_Device_type_Segment.astype('category')
 non_fraud.MU_HANDSET_MOBILE_TECH = non_fraud.MU_HANDSET_MOBILE_TECH.astype('category')
+
+fraud.drop("Telesrvc", axis=1, inplace=True)
+non_fraud.drop("Telesrvc", axis=1, inplace=True)
+
+fraud.cell_id = fraud.cell_id.fillna(method='ffill')
+fraud.MU_HANDSET_MOBILE_TECH = fraud.MU_HANDSET_MOBILE_TECH.fillna(method='ffill')
+non_fraud.cell_id = non_fraud.cell_id.fillna(method='ffill')
+non_fraud.MU_HANDSET_DUAL_SIM = non_fraud.MU_HANDSET_DUAL_SIM.fillna(method='ffill')
+non_fraud.MU_HANDSET_MOBILE_TECH = non_fraud.MU_HANDSET_MOBILE_TECH.fillna(method='ffill')
+
+fraud.sort_values(by=['Charging_Tm']).to_csv("../data/processed/1-DataCleaning/fraud.csv", index=False)
+non_fraud.sort_values(by=['Charging_Tm']).to_csv("../data/processed/1-DataCleaning/non_fraud.csv", index=False)
